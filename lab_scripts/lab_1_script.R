f0acbeb0e36985a1cfe55829d75805fb9d5b04cf
chilux00 <chiingzhang@gmail.com>
2023-09-11 14:49:36 -0700

    deleted .gitkeep and push lab 1 script upload

---
+++ lab_scripts/lab_1_script.R
@@ -0,0 +1,23 @@
+# R Labs, Week 1
+# Ching Zhang
+
+# Question 2
+# [2b] Yes
+
+# Question 3
+# [3a] body_temp_celsius
+# [3b] aspirin_dose_pt
+# [3c] tvs_pp
+# [3d] tot_height_giraffes
+
+#Question 4
+# [4a]
+15*17
+# [4b]
+13^3
+# [4c]
+log(14)
+# [4d]
+log(base=10, 100)
+# [4e]
+sqrt(81)
