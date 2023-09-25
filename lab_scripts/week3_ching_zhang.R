8a7fa4ec70f928a4553dadcc365b6924f4484d47
chilux00 <chiingzhang@gmail.com>
2023-09-24 21:32:10 -0700

    completed week 3 script and uploaded new dataset

--- lab_scripts/week3_ching_zhang.R
+++ lab_scripts/week3_ching_zhang.R
@@ -44,17 +44,69 @@ ggplot(countries,
            y = ecological_footprint_2012)) +
   geom_point() +
   xlab("Ecological footprint in 2000 (hectares)") +
-  ylab("Ecological footprint in 2012 (hectares)")
+  ylab("Ecological footprint in 2012 (hectares)") +
+  geom_abline(intercept = 0, slope = 1) 
 
 # 3b) There is a moderately strong positive and increasing relationship between 
 # the two years in terms of ecological footprint. As ecological footprint in  
 # either year increases, the ecological footprint of the other year also 
 # increases.
 
-# 3c)
+# 3c) The ecological footprint tends to go up. Countries with high ecological 
+# footprint in 2000 changed and increased the most in 2012.
 
+## Question 4
+# Life expectancy varies greatly with continent. Africa has the greatest range
+# of data points, while the Americas have the least. Outliers can be seen in 
+# the boxplots for Africa, North America and South America.
+ggplot(countries,
+       aes(x = continent,
+           y = life_expectancy_at_birth_female)) +
+  geom_boxplot() +
+  xlab("Continent") +
+  ylab("Female life expectancy at birth (years)") 
 
+## Question 5
+# 5a)
+bat_tongues <- read.csv("DataForLabs/BatTongues.csv")
+summary(bat_tongues)
 
+# 5b) The association is weak and positive.
+ggplot(bat_tongues,
+       aes(x = palate_length,
+           y = tongue_length)) +
+  geom_point() +
+  xlab("Palate Length (mm)") +
+  ylab("Tongue Length (mm)") 
 
+# 5c) The outlier must be from a different species or an individual with very 
+# long measurements.
 
+# 5d)
+library(dplyr)
 
+# 5e) The flower that this species feeds on must have a very long stigma.
+filter(bat_tongues,tongue_length>80)
+
+## Question 6
+# 6a)
+lab1 <- read.csv("DataForLabs/collecteddata_lab1.csv")
+summary(lab1)
+ggplot(lab1,
+       aes(x = Sex,
+           y = Height)) +
+  geom_boxplot() +
+  xlab("Sex") +
+  ylab("Height (cm)") 
+
+# 6b)
+ggplot(lab1,
+       aes(x = circumference_head_cm)) +
+  geom_histogram() +
+  xlab("Head circumference (cm)") 
+
+## Question 7
+# The plot made in question 4 could be better represented as a hybrid scatter 
+# and box plot combination graph to better show the distribution of data
+# within the boxes themselves, and not just the outliers
+  
