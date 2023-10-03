60003ea0c29ed6e5f8b77b743ee525e2fdf134ee
chilux00 <chiingzhang@gmail.com>
2023-10-02 20:11:45 -0700

    Solved Question 1

--- lab_scripts/week5_ching_zhang.R
+++ lab_scripts/week5_ching_zhang.R
@@ -8,4 +8,19 @@ data_plot <- data %>%
   geom_histogram(bins=50)
 data_plot
 
-# 1b) There seems to be a 
+# 1b) There seems to be an outlier of 240 cm, which is unlikely for a height
+# measurement. However, this doesn't seem like it would be a measurement error
+# in inches. There is additionally also a data point of 1.73, which seems like
+# the student has inputted an answer in meters instead of cm.
+data_filtered <- data %>%
+  filter(Height > 10, Height < 200)
+data_filtered
+
+# 1c) Mean = 168.9992 cm
+mean(data_filtered$Height, na.rm = TRUE)
+
+# 1d) SD = 8.58075 cm
+sd(data_filtered$Height, na.rm = TRUE)
+
+
+
