e044afd19388f6c9dbebe94ad05b6edcc2394410
chilux00 <chiingzhang@gmail.com>
2023-10-22 23:38:24 -0700

    solved q2

--- lab_scripts/week8_ching_zhang.R
+++ lab_scripts/week8_ching_zhang.R
@@ -9,3 +9,12 @@ hist(normal_vector)
 # 1c)
 qqnorm(normal_vector)
 
+## Question 2
+# 2a) These vector plots look more like the normal expectations than those from
+# Question 1. This is because with a larger n size, the sample size increases
+# and follows the Central Limit Theorem, which states that as the sample size 
+# increases, the distribution of the sample means will approach a normal 
+# distribution regardless of the shape of the population distribution
+normal_vector2 <- rnorm(n = 250, mean = 15, sd = 3)
+hist(normal_vector2)
+qqnorm(normal_vector2)
