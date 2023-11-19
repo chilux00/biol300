98f02291425657b53dc4be2bc7a78553ac7e451b
chilux00 <chiingzhang@gmail.com>
2023-11-18 17:24:49 -0800

    solved 1a)

---
+++ lab_scripts/week_10_ching_zhang.R
@@ -0,0 +1,16 @@
+# Ching Zhang | 68167998
+library(dplyr)
+## Question 1
+# 1a)
+cuckoo <- read.csv("DataForLabs/cuckooeggs.csv")
+cuckoo
+
+ggplot(cuckoo, aes(x = egg_length)) +   
+  geom_histogram() + 
+  facet_wrap(~ host_species, ncol = 1) +
+  theme(text = element_text(size=10)) +
+  xlab("Egg Length (mm)") +
+  ylab("Frequency")
+
+
+
